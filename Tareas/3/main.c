f895819e855104fbad7af0f1d032dc568befb7ff
Ximena-chm <54478176+Ximena-chm@users.noreply.github.com>
2019-10-16 18:14:40 -0500

    Update main.c

--- Tareas/3/main.c
+++ Tareas/3/main.c
@@ -9,9 +9,9 @@ int main (int argc, char *argv[]){
 	float divi;
 
 	sum = atoi(argv[1]) + atoi(argv[2]);
-	sum = atoi(argv[1]) + atoi(argv[2]);
-	sum = atoi(argv[1]) + atoi(argv[2]);
-	sum = atoi(argv[1]) + atoi(argv[2]);
+	rest = atoi(argv[1]) - atoi(argv[2]);
+	mult = atoi(argv[1]) * atoi(argv[2]);
+	divi = atoi(argv[1]) / atoi(argv[2]);
 
 	printf("Los resultados de las operaciones son: \n suma: %.2f \n resta: %.2f \n multiplicación: %.2f \n división: %.2f \n", sum, rest, mult, divi);
 
