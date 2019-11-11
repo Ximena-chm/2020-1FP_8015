5cabc11448b9d992e873f5b48a5b5138c0ce178b
Ximena-chm <54478176+Ximena-chm@users.noreply.github.com>
2019-11-11 15:57:50 -0600

    tarea 8

--- TAREAS/8/main.c
+++ TAREAS/8/main.c
@@ -2,20 +2,22 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main(int argc, char *argu[]){
-	foat a;
-	float b;
-	float x;
-	float y;
+int main(int argc, char *argv[]){
+	double a;
+	double b;
+	double x;
+	double y;
 
 	x=atof(argv[1]);
 	y=atof(argv[2]);
 
 	if (x==0){
+		//cambio a fahrenheit
 		a=(1.8*y)+32;
 		printf("%.2f\n", a);
 	}
 	if (x==1){
+		//cambio a centigrados
 		b=(y-32)/1.8;
 		printf("%.2f\n", b);
 	}
