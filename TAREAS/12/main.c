12975a89fa9885254eaeecec946fc1f93467efb2
Ximena-chm <54478176+Ximena-chm@users.noreply.github.com>
2019-11-12 19:05:26 -0600

    Tarea
    Tareas 12 y 9

---
+++ TAREAS/12/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//función para saber cuántos saludos se darán
+int saludos(int n){
+	int result;
+
+	if(x==2){
+		//caso base
+		result=1;
+	}
+	else{
+		//procedimiento que seguirá si no es el caso base
+		result=(n-1)+saludos(n-1);
+	}
+	return result;
+}
+
+int main (int argc, char*argu[]){
+	int result, personas;
+	personas=atoi(argu[1]);
+	// se llama a la función
+	result=saludos(personas);
+	printf("%i\n", result);
+	return 0;
+}
---
+++ TAREAS/9/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argu[]){
+	int numero;
+
+	//debemos saber cuántos caracteres tiene
+	numero=strlen(argu[1]);
+
+	//lista de caracteres crea lista
+	char argc[numero];
+	int resultado=0;
+	//nueva lista para guardar los numeros
+	int digitos[numero];
+
+	//Suma de caracteres
+	for(int i=0; i<numero; i++){
+		argc[i]=argu[1][i];//guardar en la lista de caracteres
+		digitos[i]=atoi(&arguc[i]);//guarda en la lista de numeros
+		//suma de dígitos por separado
+		resultado=resultado+digitos[i];
+	}	
+	printf("%i\n", resultado);
+	return 0;
+}
